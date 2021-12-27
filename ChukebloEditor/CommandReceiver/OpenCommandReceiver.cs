3d5069612653c26340344d148e6d32e2535927ad
chuke <ken.0416.leo@gmail.com>
2021-12-27 22:17:28 +0900

    fix unresolved error in OpenCommandReceiver

--- ChukebloEditor/CommandReceiver/OpenCommandReceiver.cs
+++ ChukebloEditor/CommandReceiver/OpenCommandReceiver.cs
@@ -1,8 +1,16 @@
-﻿namespace ChukebloEditor.CommandReceiver
+﻿using ChukebloEditor.Command;
+
+namespace ChukebloEditor.CommandReceiver
 {
     public class OpenCommandReceiver : ICommandReceiver
     {
-        public OpenParam openParam { get; }
+        public FileIOParam openParam { get; }
+
+        public OpenCommandReceiver(object param)
+        {
+            this.openParam = param as FileIOParam;
+        }
+
         public void ReceiveCommand()
         {
             throw new System.NotImplementedException();
